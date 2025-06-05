99a1e1dacdf06233f4253d11dd2a203a90d8b39a
Sebastian Kurta <KUR210534@spengergasse.at>
2025-06-05 18:35:31 +0200

    Aufgabe

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,12 +1,16 @@
-for i in range(1, 101):
-    if i % 15 == 0:
-        print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Fizz")
-    elif i % 5 == 0:
-        print("Buzz")
-    else:
-        print(i)
-
-        
-#Sebastian Kurta
+i = 1
+while True:
+    output = ""
+    if i % 3 == 0:
+        output += "Fizz"
+    if i % 5 == 0:
+        output += "Buzz"
+    if i % 7 == 0:
+        output += "Whizz"
+    if i % 11 == 0:
+        output += "Bang"
+    print(output or i)
+    if output == "FizzBuzzWhizzBang":
+        break
+    i += 1
+    #Sebastian Kurta
